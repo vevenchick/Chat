ba6314e2b1c06bf852674ee81d57a9ee402e9e10
vevenchick <vova.stoev5@gmail.com>
2019-10-03 19:07:30 +0300

    Second commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 firebase.firestore();
 function saveMessage(userName,messageText)
 {
-console.log("Сообщение " +messageText+ "отослано" +userName);
+console.log("Сообщение " +messageText+ "отослано " +userName);
 return firebase.firestore().collection( 'messages').add({ userName:userName,Text:messageText,timeStamp:new Date()});
 
 }
@@ -10,4 +10,11 @@ function sendMessage()
 var messageText=document.getElementById("messageText").value;
 var userName=document.getElementById("userName").value;
 saveMessage(userName,messageText);
+var chat=document.getElementById ("chat");
+chat.innerHTML +=`<div>
+<span class='un'>userName</span>
+<span class='tm'>-(timestamp)-</span>
+<span class='text'>messageText</span></div>`
+
 }
+
