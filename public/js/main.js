1172e864f97d0b9af31fc1c09bd88fa8d2372912
vevenchick <vova.stoev5@gmail.com>
2019-10-08 19:05:01 +0300

    show messages

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,34 @@
-firebase.firestore();
-function saveMessage(userName,messageText)
-{
-console.log("Сообщение " +messageText+ "отослано " +userName);
-return firebase.firestore().collection( 'messages').add({ userName:userName,Text:messageText,timeStamp:new Date()});
+console.log("hi!");
+var db = firebase.firestore();
+var messages = db.collection("messages").get();
+console.log(messages);
+function onGetMessages(snapshot) {
+    console.log(snapshot.docs[0].data());
 
 }
-function sendMessage()
-{
-var messageText=document.getElementById("messageText").value;
-var userName=document.getElementById("userName").value;
-saveMessage(userName,messageText);
-var chat=document.getElementById ("chat");
-chat.innerHTML +=`<div>
-<span class='un'>userName</span>
-<span class='tm'>-(timestamp)-</span>
-<span class='text'>messageText</span></div>`
+function showMessage(userName, messageText, timeStamp) {
+    var chat = document.getElementById("chat");
+    chat.innerHTML += `<div>
+    <span class='un'>`+ userName + `</span>
+    <span class='tm'>-(`+ timeStamp + `)-</span>
+    <span class='text'>`+ messageText + `</span></div>`
+}
+messages.then(onGetMessages);
+function saveMessage(userName, messageText, timeStamp) {
+    console.log("Сообщение " + messageText + "отослано " + userName);
+    return db.collection('messages').add({ userName: userName, Text: messageText, timeStamp: timeStamp });
+
+}
+function sendMessage() {
+    var messageText = document.getElementById("messageText").value;
+    var userName = document.getElementById("userName").value;
+    var timeStamp = new Date();
+    saveMessage(userName, messageText, timeStamp);
+    var chat = document.getElementById("chat");
+    chat.innerHTML += `<div>
+<span class='un'>`+ userName + `</span>
+<span class='tm'>-(`+ timeStamp + `)-</span>
+<span class='text'>`+ messageText + `</span></div>`
 
 }
 
