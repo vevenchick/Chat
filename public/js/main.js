8439bbdc75ce79247a1cd3669880488a6237c11b
vevenchick <vova.stoev5@gmail.com>
2019-10-10 16:16:45 +0300

    HHome try

--- public/js/main.js
+++ public/js/main.js
@@ -7,28 +7,30 @@ function onGetMessages(snapshot) {
 
 }
 function showMessage(userName, messageText, timeStamp) {
-    var chat = document.getElementById("chat");
-    chat.innerHTML += `<div>
-    <span class='un'>`+ userName + `</span>
-    <span class='tm'>-(`+ timeStamp + `)-</span>
-    <span class='text'>`+ messageText + `</span></div>`
-}
-messages.then(onGetMessages);
-function saveMessage(userName, messageText, timeStamp) {
-    console.log("Сообщение " + messageText + "отослано " + userName);
-    return db.collection('messages').add({ userName: userName, Text: messageText, timeStamp: timeStamp });
-
-}
-function sendMessage() {
     var messageText = document.getElementById("messageText").value;
     var userName = document.getElementById("userName").value;
     var timeStamp = new Date();
     saveMessage(userName, messageText, timeStamp);
     var chat = document.getElementById("chat");
     chat.innerHTML += `<div>
-<span class='un'>`+ userName + `</span>
+<span class='un'>`+ userName + `</span>/
 <span class='tm'>-(`+ timeStamp + `)-</span>
 <span class='text'>`+ messageText + `</span></div>`
+    for(var i=0;i<100;i++)
+    {
+        `<div><span>``</span></div>`
+    }
+
+}
+
+function saveMessage(userName, messageText, timeStamp) {
+    console.log("Сообщение " + messageText + "отослано " + userName);
+    return db.collection('messages').add({ userName: userName, Text: messageText, timeStamp: timeStamp });
+
+}
+function sendMessage() 
+{
+    
 
 }
 
