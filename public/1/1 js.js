1fe4dba09910120b909a36439fc95f68499a1d68
vevenchick <vova.stoev5@gmail.com>
2019-12-12 18:30:49 +0200

    1

---
+++ public/1/1 js.js
@@ -0,0 +1,7 @@
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+ui.start('#firebaseui-auth-container', {
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    // Other config options...
+  });
