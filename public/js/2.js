2a26ee6f23a43d8661d56cae145bf6f5c36c46d6
vevenchick <vova.stoev5@gmail.com>
2019-12-12 18:56:52 +0200

    to homework

---
+++ public/js/2.js
@@ -0,0 +1,23 @@
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+ui.start('#hi', {
+    signInOptions: [
+      firebase.auth.PhoneAuthProvider.PROVIDER_ID
+    ],
+    // Other config options...
+  });
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      // ...
+    } else {
+      // User is signed out.
+      // ...
+    }
+  });
