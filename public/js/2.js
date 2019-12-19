f06cb7d8e9d30ccbf4f5a0aa042c0ee6efde2945
vevenchick <vova.stoev5@gmail.com>
2019-12-19 16:07:32 +0200

    .

--- public/js/2.js
+++ public/js/2.js
@@ -1,25 +1,31 @@
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 ui.start('#hi', {
-    signInOptions: [
-      firebase.auth.PhoneAuthProvider.PROVIDER_ID
-    ],
-    // Other config options...
-  });
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-      var displayName = user.displayName;
-      var email = user.email;
-      var emailVerified = user.emailVerified;
-      var photoURL = user.photoURL;
-      var isAnonymous = user.isAnonymous;
-      var uid = user.uid;
-      var providerData = user.providerData;
-      var phone=user.phone;
-      document.getElementById("phone").innerHTML=phone;
-      // ...
-    } else {
-      // User is signed out.
-      // ...
-    }
-  });
+  signInOptions: [
+    firebase.auth.PhoneAuthProvider.PROVIDER_ID
+  ],
+  // Other config options...
+});
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+    var phone = user.phoneNumber;
+    document.getElementById("phone").innerHTML = phone;
+    document.getElementById('hi').style.display = 'none';
+    document.getElementById('logOut').style.display = 'block'
+    // ...
+  } else {
+    document.getElementById('hi').style.display = "visibility: visible";
+    // User is signed out.
+    // ...
+  }
+});
+function logOut() {
+  firebase.auth().signOut();
+}
