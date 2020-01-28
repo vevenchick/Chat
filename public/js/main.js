06c0b970bfaa4119961d9737d0904715d33a5cf5
vevenchick <vova.stoev5@gmail.com>
2020-01-28 13:35:26 +0200

    homework

--- public/js/main.js
+++ public/js/main.js
@@ -2,29 +2,29 @@ console.log("hi!");
 var db = firebase.firestore();
 
 var showOtherMes = db.collection("messages").orderBy("timeStamp", "desc").limit(30).onSnapshot(function (snapshot) {
-    snapshot.docChanges().reverse().forEach(function (change) {
-        if (change.type === "added") {
-            var newMes = change.doc.data();
-            console.log("New message: ", newMes);
-            showMessage(newMes.userName, newMes.Text, newMes.timeStamp)
-        }
-    })
+  snapshot.docChanges().reverse().forEach(function (change) {
+    if (change.type === "added") {
+      var newMes = change.doc.data();
+      console.log("New message: ", newMes);
+      showMessage(newMes.userName, newMes.Text, newMes.timeStamp)
+    }
+  })
 })
 function onGetMessages(snapshot) {
 
-    for (var i = snapshot.docs.length - 1; i >= 0; i--) {
-        var message = snapshot.docs[i].data();
-        showMessage(message.userName, message.Text, message.timeStamp);
-    }
+  for (var i = snapshot.docs.length - 1; i >= 0; i--) {
+    var message = snapshot.docs[i].data();
+    showMessage(message.userName, message.Text, message.timeStamp);
+  }
 }
 
 function showMessage(userName, messageText, timeStamp) {
-    var chat = document.getElementById("chat");
-    chat.innerHTML += `<div>
+  var chat = document.getElementById("chat");
+  chat.innerHTML += `<div>
     <span class='un'>`+ userName + `</span>
     <span class='tm'>-(`+ timeStamp + `)-</span>
     <span class='text'>`+ messageText + `</span></div>`
-    chat.scrollTop = 99999;
+  chat.scrollTop = 99999;
 
 }
 
@@ -32,16 +32,16 @@ function showMessage(userName, messageText, timeStamp) {
 
 
 function saveMessage(userName, messageText, timeStamp) {
-    console.log("Сообщение " + messageText + "отослано " + userName);
-    return db.collection('messages').add({ userName: userName, Text: messageText, timeStamp: timeStamp });
+  console.log("Сообщение " + messageText + "отослано " + userName);
+  return db.collection('messages').add({ userName: userName, Text: messageText, timeStamp: timeStamp });
 
 }
 
 function sendMessage() {
-    var messageText = document.getElementById("messageText").value;
-    var userName = document.getElementById("userName").value;
-    var timeStamp = new Date().toLocaleString();
-    saveMessage(userName, messageText, timeStamp);
+  var messageText = document.getElementById("messageText").value;
+  var userName = document.getElementById("userName").value;
+  var timeStamp = new Date().toLocaleString();
+  saveMessage(userName, messageText, timeStamp);
 
 }
 
@@ -64,7 +64,7 @@ function registrate() {
 
           var user = result.user;
           console.log('okey')
-
+          changeState(4);
         }).catch(function (error) {
           console.log('error')
 
@@ -91,3 +91,30 @@ window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha', {
 recaptchaVerifier.render().then(function (widgetId) {
   window.recaptchaWidgetId = widgetId;
 });
+function changeState(a) {
+  var choose = document.getElementById("choose")
+  var Registercontainer = document.getElementById("Registercontainer")
+  var chatPage = document.getElementById("chatPage")
+  var LogInContainer = document.getElementById("LogInContainer")
+  if (a == 1) {
+    choose.style.display = "none";
+    LogInContainer.style.display = "block"
+  }
+  if (a == 2) {
+    choose.style.display = "none";
+    Registercontainer.style.display = "block";
+  }
+  if (a == 3) {
+    LogInContainer.style.display = "none";
+    chatPage.style.display = "block"
+    if (a == 4) {
+      Registercontainer.style.display = "none";
+      chatPage.style.display = "block"
+
+    }
+    if (a == 5) {
+      chatPage.style.display = "none"
+      choose.style.display = "block";
+    }
+  }
+}
