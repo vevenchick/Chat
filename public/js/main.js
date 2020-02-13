b34bd15cc5bb6bf39edf89e58644e7b1ecd7d0bc
vevenchick <vova.stoev5@gmail.com>
2020-02-13 19:02:01 +0200

    sdfs

--- public/js/main.js
+++ public/js/main.js
@@ -144,3 +144,15 @@ function saveUser(user) {
        console.log(querySnapshot);
      })
 }
+function random(){
+  var a,b,c,d;
+  var ans="";
+  var all=["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M","q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m","1","2","3","4","5","6","7","8","9","0"];
+  
+  a=Math.random()*all.length;
+  b=Math.random()*all.length;
+  c=Math.random()*all.length;
+  d=Math.random()*all.length; 
+  return a
+
+  }
